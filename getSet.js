6241bbed05216ba50a45eeff79f782a4c158f7ec
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-07 15:41:35 +0200

    Anti-Function get/set in Task 1

--- getSet.js
+++ getSet.js
@@ -1,7 +1,7 @@
 var generator = {
     getSetGen: function () {
         for (var item in this) {
-            if (item != "getSetGen") {
+            if (item != "getSetGen" && typeof this[item] !="function") {
                 this["get" + item] =
                     (function (itm) {
                         return function () {
@@ -23,4 +23,6 @@ var student = {};
 student.name = "mario";
 student.age = 22;
 student.tel = "01288233002";
+student.f = function() {console.log("hello");}
+
 generator.getSetGen.apply(student);
